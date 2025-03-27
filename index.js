751597232a3a2501df2f73040626276b60886b86
vasilnedev <vasilnedev55@gmail.com>
2025-03-27 10:00:42 +0000

    Refined form events

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function showModal(id) {
+const showModal= id => {
   const modal = document.getElementById(id)
   if (modal) {
     modal.showModal()
@@ -10,17 +10,26 @@ function showModal(id) {
 // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
 const newProjectBtn = document.getElementById("new-project-btn")
 if (newProjectBtn) {
-  newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
+  newProjectBtn.addEventListener("click", () => showModal("new-project-modal"))
 } else {
   console.warn("New projects button was not found")
 }
 
 const projectForm = document.getElementById("new-project-form")
 if (projectForm) {
+  projectForm.addEventListener("reset", (e) => {
+    projectForm.parentElement.close()
+  })
   projectForm.addEventListener("submit", (e) => {
     e.preventDefault()
+    let msg='You enerered the following data:\n'
     const formData = new FormData(projectForm)
-    console.log("Description:", formData.get("description"))
+    formData.forEach((value, key) => {
+      msg+=`${key}: ${value}\n`
+    })
+    projectForm.reset()
+    alert(msg)
+    projectForm.parentElement.close()
   })
 } else {
 	console.warn("The project form was not found. Check the ID!")
