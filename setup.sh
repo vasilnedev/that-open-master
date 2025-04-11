864e28cb11fbbc6318f1cbb0c1cf91c4a4786ffd
vasilnedev <vasilnedev55@gmail.com>
2025-04-11 21:25:15 +0100

    Correction to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Step 1. Pull Docker images..."
 docker image pull node
 
 echo "Step 2. Install node modules..."
-docker run -v $(pwd)/bim-app:/usr/src/app -w /usr/src/app node npm install
+docker run -v $(pwd):/usr/src/app -w /usr/src/app node npm install
 
 echo "Step 3. Starting Docker containers..."
 docker compose up -d
